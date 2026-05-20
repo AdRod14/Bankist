715e958c03fdb1547376d6c43a537baa780ef77e
AdRod14 <adan.rodarte@uabc.edu.mx>
2026-05-19 17:52:58 -0700

    Added section 12 starter code

--- script.js
+++ script.js
@@ -4,12 +4,29 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
+/////////////////////////////////////////////////
 // Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,24 +34,24 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+const accounts = [account1, account2];
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
+/////////////////////////////////////////////////
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
@@ -61,165 +78,175 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+/////////////////////////////////////////////////
+// Functions
 
-const displayMovements = function(movements, sort = false) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a,b) => a - b) : movements 
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
 
-  movs.forEach(function(mov, i) {
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
     const html = `
       <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
         <div class="movements__value">${mov}€</div>
       </div>
     `;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
 
-  })
-}
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
+};
 
-
-const calcPrintBalance = function(acc){
-  acc.balance = acc.movements.reduce((acc, curr) => acc + curr, 0);
-  labelBalance.textContent = `${acc.balance} €`;
-}
-
-
-const calcDisplaySummary = function(acc) {
-  const incomes = acc.movements.filter(mov => mov > 0).reduce((acc, curr) => acc + curr, 0);
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = acc.movements.filter((mov) => mov < 0).reduce((acc, curr) => acc + curr, 0);
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
   const interest = acc.movements
-  .filter(mov => mov > 0)
-  .map((deposit) => deposit*(acc.interestRate/100))
-  .filter((int, i, arr) => {
-    console.log(arr);
-    return int >= 1;
-  })
-  .reduce((acc, interest) => acc + interest, 0);
-  labelSumInterest.textContent = `${interest}€`
-}
-
-
-const createUsernames = function(accs) {
-  accs.forEach(function(acc) {
-    acc.username = acc.owner
-    .toLowerCase()
-    .split(' ')
-    .map((name) => name[0])
-    .join('');
-  })
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      // console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
 };
 
-
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
 createUsernames(accounts);
 
-
-const updateUI = function(acc) {
-  //Display movements
+const updateUI = function (acc) {
+  // Display movements
   displayMovements(acc.movements);
 
+  // Display balance
+  calcDisplayBalance(acc);
 
-  //Display balance
-  calcPrintBalance(acc);
-
-  //Display summary
+  // Display summary
   calcDisplaySummary(acc);
-}
+};
 
-
-
-//Event handler
-let currentAccount ;
+///////////////////////////////////////
+// Event handlers
+let currentAccount;
 
 btnLogin.addEventListener('click', function (e) {
-  //Prevent form from submitting
+  // Prevent form from submitting
   e.preventDefault();
 
-  currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
   console.log(currentAccount);
 
-  if(currentAccount?.pin === Number(inputLoginPin.value)){
-    //Display UI and message
-    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`;
-    containerApp.style.opacity = 1;
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
 
-    //Clear input fields
+    // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
-    //update UI
+    // Update UI
     updateUI(currentAccount);
   }
 });
 
-
-btnTransfer.addEventListener('click', function(e) {
+btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
-  const receiverAcc = accounts.find((acc) => acc.username === inputTransferTo.value);
-  console.log(amount, receiverAcc);
-
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
   inputTransferAmount.value = inputTransferTo.value = '';
 
-  if(amount > 0 && receiverAcc && currentAccount.balance >= amount && receiverAcc?.username !== currentAccount.username){
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
+
+    // Update UI
     updateUI(currentAccount);
   }
-
 });
 
-
-btnLoan.addEventListener('click', function(e) {
+btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
   const amount = Number(inputLoanAmount.value);
 
-  if(amount > 0 && currentAccount.movements.some(mov => mov >= amount*0.1)){
-    //Add movement
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
     currentAccount.movements.push(amount);
 
-    //update UI
+    // Update UI
     updateUI(currentAccount);
   }
-
   inputLoanAmount.value = '';
-
 });
 
-
-btnClose.addEventListener('click', function(e) {
+btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
-  if(inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin){
-    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
     console.log(index);
+    // .indexOf(23)
 
-    //Delete account
+    // Delete account
     accounts.splice(index, 1);
 
     // Hide UI
     containerApp.style.opacity = 0;
   }
 
-  inputClosePin.value = inputCloseUsername.value = '';
-
+  inputCloseUsername.value = inputClosePin.value = '';
 });
 
-
 let sorted = false;
-btnSort.addEventListener('click', function(e){
+btnSort.addEventListener('click', function (e) {
   e.preventDefault();
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
-})
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -502,4 +529,122 @@ movements.sort((a,b) => a - b);
 movements.sort((a,b) => b - a);
 console.log(movements);
 
+
+
+///////////////  ARRAY GROUPING ///////////////
+console.log(movements);
+
+const groupedMovements = Object.groupBy(movements, (mov) => mov > 0 ? 'deposits':'withdrawals');
+console.log(groupedMovements);
+
+const groupedByActivity = Object.groupBy(accounts, account => {
+  const movementCount = account.movements.length;
+  if(movementCount >= 8) return 'very active';
+  if(movementCount >= 4) return 'active';
+  if(movementCount >= 1) return 'moderate';
+  return 'inactive';
+});
+
+console.log(groupedByActivity);
+
+// const groupedAccounts = Object.groupBy(accounts, (account) => account.type);
+const groupedAccounts = Object.groupBy(accounts, ({type}) => type);
+
+console.log(groupedAccounts);
+
+
+/////////////////////////// MORE WAYS OF CREATING AND FILLING ARRAYS //////////////////////
+
+// FILL METHOD
+const x = new Array(7); //Creates empty array of length 7
+console.log(x);
+x.map(() => 5);   //Cant use methods on empty array, only fill
+//x.fill(1);
+x.fill(1,3,5);
+console.log(x);
+
+
+const arr = [1,2,3,4,5,6,7];
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// ARRAY.FROM
+const y = Array.from({length: 7}, () => 1);
+console.log(y);
+
+const z = Array.from({length: 7}, (cur, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function(e) {
+  const movementsUI = Array.from(document.querySelectorAll('.movements__value'), (el) => Number(el.textContent.replace('€', '')));
+  // movementsUI2 = [...document.querySelectorAll('.movements__value')];
+  console.log(movementsUI);
+});
+
+
+///////////// DESTRUCTIVE VS NON DESTRUCTIVE METHODS ///////////
+
+//DO MUTATE REVERSE
+console.log(movements);
+const reversedMov = movements.reverse()
+console.log(reversedMov);
+console.log(movements);
+
+//ALTERNATIVE FOR REVERSE
+console.log(movements);
+const reversedMov2 = movements.toReversed()
+console.log(reversedMov2);
+console.log(movements);
+
+// DO MUTATE SORT
+//ALTERNATIVE FOR SORT - toSorted
+
+// DO MUTATE SPLICE
+//ALTERNATIVE FOR SPLICE - toSpliced
+
+
+//To change an element in array without modifying original
+//movements[1] = 2000;
+const newMovements = movements.with(1, 2000);
+console.log(newMovements);
+console.log(movements);
+
+
+//////////////////////// CONVERTING AND CHECKING NUMBERS ///////////////////////
+// Base 10 - 0 to 9. 1/10 = 10.1  3/10=3.3333
+//Binary base 2 - 0 to 1
+//JS not useful for precise calculations
+console.log( 23 === 23.0);
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3); //Gives false
+
+//Converting to number
+console.log(Number('23'));
+console.log(+('23')); //Type coercion
+
+//Parsing - needs to start with a number
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e23', 10));
+console.log(Number.parseInt('30px', 4));
+
+console.log(Number.parseFloat('2.5rem'));
+console.log(Number.parseInt('2.5rem'));
+
+
+//NaN - use to check if value is literally NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(10/0));    //Doesn't consider infinity value
+console.log(10 / 0);
+
+//isFinite -  best way of checking if it is a number, not a string
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20x'));
+console.log(Number.isFinite(10/0));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23.01));
 */
